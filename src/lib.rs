95ef50f24a6bb1e7406d5bc4cc1377b88c4a0749
SnefDenGames <snefden.games@gmail.com>
2021-09-05 01:50:56 +0200

    add all

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! # data structures
+//! 
+//! `data_structures` is a collection of data structures.
+//! That containing as much structures for any kind of data as possible.
+
+/// containing any kind of lists
+pub mod lists;
